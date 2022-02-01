1698f16b520b1ce8e8ba0221bc397d2417b22b84
hollowblind095 <hollowblind095@gmail.com>
2022-02-01 20:37:25 -0300

    proyect template

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css'
+
+const Footer = () => {
+  return <footer className="footer-title">Soy un Footer</footer>;
+};
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css'
+
+const Navbar = () => {
+  return <nav className="navbar-title">Soy la Navbar</nav>;
+};
+
+export default Navbar;
