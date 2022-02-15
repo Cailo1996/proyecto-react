8830051d055ec7b76abed56b6a8b62c973888bc4
Matias Cailotto <82989529+Cailo1996@users.noreply.github.com>
2022-02-15 15:15:36 -0300

    Warning

--- src/components/Warning/Warning.jsx
+++ src/components/Warning/Warning.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import './Styles.css';
+import React from "react";
+import { XIcon } from "@heroicons/react/outline";
 
 const Warning = () => {
-    return ( 
-        <p>Warning</p>
-     );
-}
- 
+  return (
+    <div className="bg-stone-400 flex flex-row  ">
+      <p className="font-medium text-white grow flex justify-center ">
+        Free Delivery over $100. Gift with Purchase over $150
+      </p>
+      <button className="" type="button">
+        <XIcon className="h-4 w-4 text-white mx-4 " />
+      </button>
+    </div>
+  );
+};
+
 export default Warning;
