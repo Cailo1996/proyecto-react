69861ce7887f62dde4021f3c8945f37a279d3d75
Matias Cailotto <82989529+Cailo1996@users.noreply.github.com>
2022-02-15 19:36:45 -0300

    Warning

--- src/components/Warning/Warning.jsx
+++ src/components/Warning/Warning.jsx
@@ -1,17 +1,36 @@
 import React from "react";
-import { XIcon } from "@heroicons/react/outline";
 
 const Warning = () => {
+  const [showAlert, setShowAlert] = React.useState(true);
   return (
-    <div className="bg-stone-400 flex flex-row  ">
-      <p className="font-medium text-white grow flex justify-center ">
-        Free Delivery over $100. Gift with Purchase over $150
-      </p>
-      <button className="" type="button">
-        <XIcon className="h-4 w-4 text-white mx-4 " />
-      </button>
-    </div>
+    <>
+      {showAlert ? (
+        <div className="bg-stone-400 flex flex-row ">
+          <p className="font-semibold text-white grow flex justify-center  ">
+            Free Delivery over $100. Gift with Purchase over $150
+          </p>
+          <button
+            className="text-white text-2xl font-semibold align-middle mr-4 "
+            onClick={() => setShowAlert(false)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+      ) : null}
+    </>
   );
 };
-
 export default Warning;
