e8a34c8c8346bd05a1f9a454f2046d48d06c89d9
Gabriel Farfán <gmfarfan85@gmail.com>
2022-02-22 19:00:14 -0300

    Why Choose modificacion
    Why Choose modificacion

--- src/components/Why Choose/WhyChoose.jsx
+++ src/components/Why Choose/WhyChoose.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
 
-
-
-
 const WC = ({list}) => {
     return (
         <div className="bg-white">
           <div className="max-w-2xl px-4 py-4 mx-auto sm:px-6 lg:max-w-7xl lg:px-4">
-            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Why Choose<span className="text-orange-600"> the Premium Bakery Quality</span></h2>
+            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 font-serif text-center">Why Choose<span className="text-orange-600 font-serif"> the Premium Bakery Quality </span></h2>
     
             <div className="grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-8">
               {list.map((item, index) => (
-                <div key={index} className="relative shadow-xl group rounded-xl shadow-gray-500">
-                  <div className="w-full min-h-80 aspect-w-1 aspect-h-1 lg:h-80 lg:aspect-none">
+                <div key={index} className="relative group">
+                  <div className="w-full min-h-20 aspect-w-1 aspect-h-1 lg:h-20 lg:aspect-none">
                     <img
                       src={item.icono}
                       alt={item.nombre}
-                      className="object-cover object-center w-full h-64 rounded-t-xl sm:h-80 "
+                      className=" w-14 "
                     />
                   </div>
-                  <div className="flex justify-around my-4 ">
+                  <div className="flex-col ">
                     <div>
-                      <h3 className="font-bold text-gray-900 lg:text-sm">{item.nombre}</h3>
+                      <h3 className="font-bold text-gray-900 lg:text-lg font-serif">{item.nombre}</h3>
                     </div>                  
-                    <p className="font-bold text-gray-900 lg:text-sm">{item.descripcion} <span className="text-sm font-medium text-gray-500 line-through lg:text-xs">{item.antes}</span></p>
+                    <p className=" text-gray-900 lg:text-base font-serif">{item.descripcion} <span className="text-sm font-medium text-gray-500 line-through lg:text-xs font-serif">{item.antes}</span></p>
                   </div>
                   
                 </div>
