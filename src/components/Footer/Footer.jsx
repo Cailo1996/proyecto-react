4102788550dbda0176d0c04d736b2c452e016718
claudiafe02 <claudiafe@yahoo.com>
2022-02-23 18:23:36 -0300

    Opciones footer y margenes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import footerlist from './footerlist/footerlist'
-// import './styles.css'
+import './styles.css'
 
 
 const Footer = () => {
   return (
-      {/* <div className="flex flex-row space-x-20 footer-title"> */}
-    {
-      <div className="flex flex-row h-screen py-6 bg-grey-900">
-        <div class="basis-1/4"></div>
+      
+    <div className="flex flex-row h-screen py-6 m-6 bg-slate-200">
+      <div class="basis-1/4"></div>
         <div class="basis-1/4"><h2>Ocassions</h2></div>
         <div class="basis-1/4"><h2>All Categories</h2></div>
         <div class="basis-1/4"><h2>Gifts & Bundles</h2></div>
         <div class="basis-1/4"><h2>Our Brands</h2></div>
         <div class="basis-1/4"><h2>About Us</h2></div>
-      </div>
-    }
-     )
+    </div>
+  
+  );
   
   
   
