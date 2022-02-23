7755b659276d9326417b198cf7c594ede324fa6a
claudiafe02 <claudiafe@yahoo.com>
2022-02-23 15:14:05 -0300

    commit para eliminar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import './styles.css'
+import footerlist from './footerlist/footerlist'
+// import './styles.css'
+
 
 const Footer = () => {
-  return <footer className="footer-title">Soy un Footer</footer>;
+  return (
+      {/* <div className="flex flex-row space-x-20 footer-title"> */}
+    {
+      <div className="flex flex-row h-screen py-6 bg-grey-900">
+        <div class="basis-1/4"></div>
+        <div class="basis-1/4"><h2>Ocassions</h2></div>
+        <div class="basis-1/4"><h2>All Categories</h2></div>
+        <div class="basis-1/4"><h2>Gifts & Bundles</h2></div>
+        <div class="basis-1/4"><h2>Our Brands</h2></div>
+        <div class="basis-1/4"><h2>About Us</h2></div>
+      </div>
+    }
+     )
+  
+  
+  
+  
+  // <footer className="footer-title">Soy un Footer</footer>;
 };
 
 export default Footer;
