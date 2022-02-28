e8f06889c4a1af99dc35e4956fa412cca2086346
Ivangold91 <89114562+Ivangold91@users.noreply.github.com>
2022-02-27 21:19:09 -0300

    cambios tell a friend

--- src/components/Tell a Friend/TellaFriend.jsx
+++ src/components/Tell a Friend/TellaFriend.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import './Styles.css';
+// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+// import {fa-facebook, fa-instagram-square, fa-whatsapp-square} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {} from '@fortawesome/free-solid-svg-icons';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+// import './Styles.css';
 
 const TellaFriend = () => {
     return (
-        <p>Tell a Friend</p>
+      <>
+      <div>
+          <h2>Tell a Friend</h2>
+          
+          <li>  <FontAwesomeIcon icon={faFacebook} />  </li>
+         
+          {/* <ul className='flex flex-row'>
+              <li> <FontAwesomeIcon icon={fa-facebook} />           </li>
+              <li> <FontAwesomeIcon icon={fa-instagram-square} />         </li>
+              <li> <FontAwesomeIcon icon={fa-whatsapp-square} />         </li>  
+          </ul> */}
+      </div>
+      </>
       );
 }
  
---
+++ src/data/tellafriend.js
@@ -0,0 +1,12 @@
+// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+// import {faMagnifyingGlass, faUser, faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+
+
+// export const tellafriend = [
+//     {   socialnet: "Facebook",    link: "https://www.facebook.com/",    icon: },
+//     {   socialnet: "Instagram",   link: "https://www.instagram.com/",   icon: },
+//     {   socialnet: "Twitter",     link: "https://www.facebook.com/",    icon: },
+//     {   socialnet: "Whatsapp",    link: "https://www.facebook.com/",    icon: },
+//     {   socialnet: "Messenger",   link: "https://www.facebook.com/",    icon: },
+//     {   socialnet: "Telegram",    link: "https://www.facebook.com/",    icon: },
+//   ];
