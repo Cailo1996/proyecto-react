28de14fa2b63c8d7397ba0acbf05d85dfebc7af7
claudiafe02 <claudiafe@yahoo.com>
2022-03-01 12:12:48 -0300

    Agrego columnas y muestro el primer arreglo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,20 @@ import { footList } from '../../data/fdata';
 const Footer = () => {
   return (
     <>
-    <div>
-    {footList.map((item) => (
+    <div className='h-screen pt-8 m-10 bg-orange-50'>
+      <div className='flex space-x-40 text-base py-6 font-semibold'>
+        <div >         </div>
+        <div >Occasions</div>
+        <div >All Categories</div>
+        <div >Gift & Bundles</div>
+        <div >Our Brands</div>
+        <div >About Us</div>
+      </div>
+
+    {footList.map((item) => ( 
             
                 <ul>
-                  <li>{item.description}</li>
+                  <li className='mx-40 text-base font-light'>{item.description}</li>
                 </ul>
             
         ))}
