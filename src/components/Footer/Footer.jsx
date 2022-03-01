0a6e34f368ad1cff91b861a1862ebb1880b420ac
claudiafe02 <claudiafe@yahoo.com>
2022-03-01 20:35:03 -0300

    Agregué carpeta y logo jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
 import { footList } from '../../data/fdata';
+import Logo from './Logo/Logo';
+
 
 const Footer = () => {
   return (
     <>
-    <div className='h-screen pt-8 m-10 bg-orange-50'>
-      <div className='flex space-x-40 text-base py-6 font-semibold'>
-        <div >         </div>
-        <div >Occasions</div>
-        <div >All Categories</div>
-        <div >Gift & Bundles</div>
-        <div >Our Brands</div>
-        <div >About Us</div>
-      </div>
+      <div className='h-screen pt-8 m-4 bg-orange-50'>
 
-    {footList.map((item) => ( 
-            
-                <ul>
-                  <li className='mx-40 text-base font-light'>{item.description}</li>
-                </ul>
-            
-        ))}
-    </div>
+        <div className='grid grid-cols-6 pl-10 text-base font-semibold'>
+
+        {/* <div className='flex space-x-40 text-base py-6 font-semibold'> */}
+            <div ><Logo/></div>
+            <div className='text-left'>Occasions</div>
+            <div >All Categories</div>
+            <div >Gift & Bundles</div>
+            <div >Our Brands</div>
+            <div >About Us</div>
+        </div>
+
+          {footList.map((item) => ( 
+              <div className='grid grid-cols-6 text-base-base font-light'>
+                <div className='col-start-2 text-left'>{item.description}</div>
+                <div className='col-start-3 text-left'>xxxxxxxxxxxxxxxxx</div>
+                <div className='col-start-4 col-span-6 text-left'>xxxxxxxxxxxxxxxxx</div>
+              
+                {/* <ul> */}
+                  {/* <li className='px-40 text-base font-light'>{item.description}</li> */}
+                  {/* <li >{item.description}</li> */}
+                {/* </ul> */}
+              </div>
+         ))}
+      </div>
     </>
   ) 
  };
