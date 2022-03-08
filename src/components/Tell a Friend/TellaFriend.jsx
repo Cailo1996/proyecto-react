ace138b8302fce5d266ca504de31c0e6ed66639f
Ivangold91 <89114562+Ivangold91@users.noreply.github.com>
2022-03-08 20:20:51 -0300

    dev-ivan

--- src/components/Tell a Friend/TellaFriend.jsx
+++ src/components/Tell a Friend/TellaFriend.jsx
@@ -3,22 +3,27 @@ import React from 'react';
 // import {fa-facebook, fa-instagram-square, fa-whatsapp-square} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {} from '@fortawesome/free-solid-svg-icons';
-import {faFacebook} from '@fortawesome/free-brands-svg-icons';
-// import './Styles.css';
+import {tellafriends} from '../../data/tellafriend'
+
 
 const TellaFriend = () => {
     return (
       <>
-      <div>
-          <h2>Tell a Friend</h2>
-          
-          <li>  <FontAwesomeIcon icon={faFacebook} />  </li>
-         
-          {/* <ul className='flex flex-row'>
-              <li> <FontAwesomeIcon icon={fa-facebook} />           </li>
-              <li> <FontAwesomeIcon icon={fa-instagram-square} />         </li>
-              <li> <FontAwesomeIcon icon={fa-whatsapp-square} />         </li>  
-          </ul> */}
+      <div className='bg-yellow-200'>
+            <div className='flex justify-center mb-6'> 
+                <h2>Tell a Friend</h2>
+            </div> 
+                            
+            <ul className='flex flex-row justify-center'>
+                 {
+                  tellafriends.map(listfriend => (
+                    <li className='px-8'>     
+                        <a href={listfriend.link}> <FontAwesomeIcon icon={listfriend.icon} />  </a>  
+                    </li>
+                  ))  
+                }
+            </ul>
+
       </div>
       </>
       );
--- src/data/tellafriend.js
+++ src/data/tellafriend.js
@@ -1,12 +1,43 @@
 // import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-// import {faMagnifyingGlass, faUser, faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+// import {} from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebookMessenger,
+  faFacebookSquare,
+  faInstagramSquare,
+  faTelegram,
+  faTwitterSquare,
+  faWhatsappSquare,
+} from "@fortawesome/free-brands-svg-icons";
 
-
-// export const tellafriend = [
-//     {   socialnet: "Facebook",    link: "https://www.facebook.com/",    icon: },
-//     {   socialnet: "Instagram",   link: "https://www.instagram.com/",   icon: },
-//     {   socialnet: "Twitter",     link: "https://www.facebook.com/",    icon: },
-//     {   socialnet: "Whatsapp",    link: "https://www.facebook.com/",    icon: },
-//     {   socialnet: "Messenger",   link: "https://www.facebook.com/",    icon: },
-//     {   socialnet: "Telegram",    link: "https://www.facebook.com/",    icon: },
-//   ];
+export const tellafriends = [
+  {
+    socialnet: "Facebook",
+    link: "https://www.facebook.com/",
+    icon: faFacebookSquare,
+  },
+  {
+    socialnet: "Instagram",
+    link: "https://www.instagram.com/",
+    icon: faInstagramSquare,
+  },
+  {
+    socialnet: "Twitter",
+    link: "https://www.facebook.com/",
+    icon: faTwitterSquare,
+  },
+  {
+    socialnet: "Whatsapp",
+    link: "https://www.facebook.com/",
+    icon: faWhatsappSquare,
+  },
+  {
+    socialnet: "Messenger",
+    link: "https://www.facebook.com/",
+    icon: faFacebookMessenger,
+  },
+  {
+    socialnet: "Telegram",
+    link: "https://www.facebook.com/",
+    icon: faTelegram,
+  },
+];
